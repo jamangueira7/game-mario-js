4c81c6b8a7d212e17e4174828953eebd381d8f8c
João Mangueira <jamjs7@gmail.com>
2022-08-15 16:09:30 -0300

    add efeito parallax

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,4 +1,7 @@
 import platform from '../img/platform.png';
+import hills from '../img/hills.png';
+import background from '../img/background.png';
+
 const canvas = document.querySelector('canvas');
 
 const context = canvas.getContext('2d');
@@ -65,12 +68,57 @@ class Platform {
   }
 }
 
-const image = new Image();
-image.src = platform
+class GenericObject {
+  constructor({ x, y, image }) {
+    this.position = {
+      x: x,
+      y: y
+    };
+    this.image = image;
+    this.width = image.width;
+    this.height = image.height;
+  }
+
+  draw() {
+    context.drawImage(
+        this.image,
+        this.position.x,
+        this.position.y
+    );
+  }
+}
+function createImage(imageSrc) {
+  const image = new Image();
+  image.src = imageSrc;
+  return image;
+}
+
+const platformImage = createImage(platform);
+
 const player = new Player();
 const platforms = [
-  new Platform({ x: -1, y: 470, image: image}),
-  new Platform({ x: image.width-3, y: 470, image: image}),
+  new Platform({
+    x: -1,
+    y: 470,
+    image: platformImage
+  }),
+  new Platform({
+    x: platformImage.width-3,
+    y: 470,
+    image: platformImage}),
+];
+
+const genericObjects = [
+  new GenericObject({
+    x: -1,
+    y: -1,
+    image: createImage(background)
+  }),
+  new GenericObject({
+    x: -1,
+    y: -1,
+    image: createImage(hills)
+  }),
 ];
 
 const keys = {
@@ -94,6 +142,10 @@ function animate() {
       canvas.height
   );
 
+  genericObjects.forEach((genericObject) => {
+    genericObject.draw();
+  })
+
   platforms.forEach((platform) => {
     platform.draw();
   });
@@ -110,10 +162,19 @@ function animate() {
       platforms.forEach((platform) => {
         platform.position.x -= 5;
       });
+
+      genericObjects.forEach((genericObject) => {
+        genericObject.position.x -= 3;
+      });
+
     } else if (keys.left.pressed) {
       scrollOffset -= 5
       platforms.forEach((platform) => {
         platform.position.x += 5;
+      });
+
+      genericObjects.forEach((genericObject) => {
+        genericObject.position.x += 3;
       });
     }
   }
